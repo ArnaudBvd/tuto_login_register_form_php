bb3206fe822bf881ba448730ef997e9f918688f1
ArnaudBvd <bravard.arnaud@gmail.com>
2023-06-11 10:38:51 +0200

    création du formulaire d'enregistrement sur la page register.php

---
+++ register.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Créer un compte</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Créer un compte</h1>
+
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group mt-3">
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" name="password" placeholder="Mot de passe" class="form-control">
+                <label for="password2">Confirmation du mot de passe</label>
+                <input type="password2" id="password2" name="password2" placeholder="Confirmez le mot de passe" class="form-control">
+            </div>
+            <input type="submit" class="btn btn-success my-3" value="Envoyer">
+        </form>
+
+        <a href="login.php">Me connecter</a>
+    </div>
+</body>
+
+</html>
